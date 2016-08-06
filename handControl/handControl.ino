be792ba367b552c39e14463e7fa74d281d363281
Nitesh Kartha <karthanitesh@gmail.com>
2016-08-06 10:18:44 -0500

    Code for Initial Values

---
+++ handControl/handControl.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  myservo.write(20); //lower bound 20
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+}
